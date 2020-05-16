f7ade8ec9dff99095eb159af9a886cb5b8953c7b
PiedeGrande <bigfoot4916@gmail.com>
2020-05-16 15:43:12 +0200

    Stuff

--- release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-PROJECT_NAME=UpdateTest
-PROJECT_DIR=$(pwd)/$PROJECT_NAME
-INFOPLIST_FILE="Info.plist"
-
-CFBundleVersion=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${PROJECT_DIR}/$PROJECT_NAME/${INFOPLIST_FILE}")
-CFBundleShortVersionString=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "${PROJECT_DIR}/$PROJECT_NAME/${INFOPLIST_FILE}")
-
-zip -r "Product/$PROJECT_NAME.v${CFBundleShortVersionString}.b${CFBundleVersion}.zip" Product/$PROJECT_NAME.app
